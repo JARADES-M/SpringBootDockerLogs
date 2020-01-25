0e074576b9f9ef40593a1f3f644fdb5470bf7920
Jarades Monteiro <jaradesm@gmail.com>
2020-01-25 13:48:46 -0300

    Remove default logging. Add SLF4J/LOG4J2

--- src/main/java/hello/controller/HelloController.java
+++ src/main/java/hello/controller/HelloController.java
@@ -1,15 +1,19 @@
 package hello.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+    private static final Logger LOGGER = LoggerFactory.getLogger(HelloController.class);
 
-    public static final String DEFAULT_RETURN_STRING = "Hello World Docker";
+    public static final String DEFAULT_RETURN_STRING = "My Hello World Docker";
 
     @GetMapping("/")
     public String home() {
+        LOGGER.info("home called!");
         return DEFAULT_RETURN_STRING;
     }
 
