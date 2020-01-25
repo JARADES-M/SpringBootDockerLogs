0de90ac08343fa9cc089c7ea7053493f388f38d1
Jarades Monteiro <jaradesm@gmail.com>
2020-01-25 13:59:42 -0300

    Update logs

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -1,11 +1,15 @@
 package hello;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
+	private static final Logger LOGGER = LoggerFactory.getLogger(Application.class);
 	public static void main(String[] args) {
+		LOGGER.info("{} is about to run {}!", "Application", "...");
 		SpringApplication.run(Application.class, args);
 	}
 
